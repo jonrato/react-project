ce1b8331e6f951df31159209fddece7f7e09096a
jon <jonatas_ferreira@usp.br>
2021-10-11 14:41:59 -0300

    components React

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,20 +1,21 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
+  }
 }
-
 export default App;
+
